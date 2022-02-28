5d2b081570d487b5f52f00b406f4516c16bd61e2
Gabriela <gaabrielaalmeida16@gmail.com>
2022-02-28 16:06:15 -0300

    inicio testes delete subprojeto

---
+++ DesafioAPI/Tests/Projects/DeleteASubprojectTest.cs
@@ -0,0 +1,35 @@
+﻿using DesafioAPI.Helpers.Project;
+using DesafioAPI.Requests.Projects;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharpNetCoreTemplate.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioAPI.Tests.Projects
+{
+    [TestFixture]
+    public class DeleteASubprojectTest : TestBase
+    {
+        private Project projeto;
+        private Project subprojeto;
+
+        [SetUp]
+        public void Init()
+        {
+            ProjectHelpers projectHelpers = new ProjectHelpers();
+            projeto = projectHelpers.CriarProjetoDefault();
+            subprojeto = projectHelpers.CriarProjetoDefault();
+
+            projectHelpers.AssociarSubProjeto(projeto, subprojeto);
+        }
+
+        [Test]
+        public void DeletarUmSubprojeto()
+        {
+            
+        }
+    }
+}
