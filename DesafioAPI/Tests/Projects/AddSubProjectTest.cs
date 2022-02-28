e8f96d7a14e7b09f90d4a78583eb4fa1632cf86a
Gabriela <gaabrielaalmeida16@gmail.com>
2022-02-28 00:07:51 -0300

    teste adicionar subprojeto usando o projecthelpers

--- DesafioAPI/Tests/Projects/AddSubProjectTest.cs
+++ DesafioAPI/Tests/Projects/AddSubProjectTest.cs
@@ -1,4 +1,5 @@
-﻿using DesafioAPI.Requests.Projects;
+﻿using DesafioAPI.Helpers.Project;
+using DesafioAPI.Requests.Projects;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using RestSharp;
@@ -32,9 +33,34 @@ namespace DesafioAPI.Tests.Projects
             Assert.AreEqual(System.Net.HttpStatusCode.NoContent, response.StatusCode);
             //Assert.AreEqual("1", response.Data["id"].ToString());
 
+        }
+        [Test]
+        public void AdicionarSubProjeto()
+        {
+            // Criar Projeto e SubProjeto para utilizar nos testes
+            ProjectHelpers projectHelpers = new ProjectHelpers();
+            Project projeto = projectHelpers.CriarProjetoDefault();
+            Project subprojeto = projectHelpers.CriarProjetoDefault();
 
+            // Associar o SubProjeto ao Projeto
+            projectHelpers.AssociarSubProjeto(projeto, subprojeto);
 
+            // Consultar o projeto criado acima pelo seu próprio Id
+            Project project = projectHelpers.ObterProjetoPorId(projeto.Id);
+
+            // Verificar se existe algum subProjeto associado ao projeto consultado
+            Assert.IsTrue(project.SubProjects.Count > 0);
+
+            // Obter o primeiro subProjeto do Projeto consultado 
+            Project subproj = project.SubProjects[0];
+
+            // Validar se o subProjeto criado é o mesmo subprojeto consultado 
+            Assert.AreEqual(subprojeto.Id, subproj.Id);
+
+            Console.WriteLine(project.Name);
+            Console.WriteLine(subproj.Name);
 
         }
+
     }
 }
