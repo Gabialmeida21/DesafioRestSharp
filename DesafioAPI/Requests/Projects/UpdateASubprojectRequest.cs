ffdfaa88d2d3e30f5620e3aeadcb999a8dc08475
Gabriela <gaabrielaalmeida16@gmail.com>
2022-02-28 00:02:42 -0300

    teste atualizar subprojeto

---
+++ DesafioAPI/Requests/Projects/UpdateASubprojectRequest.cs
@@ -0,0 +1,27 @@
+﻿using RestSharpNetCoreTemplate.Bases;
+using RestSharpNetCoreTemplate.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DesafioAPI.Requests.Projects
+{
+    public class UpdateASubprojectRequest : RequestBase
+    {
+        public UpdateASubprojectRequest(int project_id, int subproject_id)
+        {
+            method = RestSharp.Method.PATCH;
+            requestService = "/api/rest/projects/{project_id}/subprojects/{subproject_id}";
+
+            parameters.Add("project_id", project_id.ToString());
+            parameters.Add("subproject_id", subproject_id.ToString());
+        }
+
+        public void SetJsonBody(string name)
+        {
+            jsonBody = File.ReadAllText(GeneralHelpers.ReturnProjectPath() + "Jsons/Projects/UpdateASubproject.json", Encoding.UTF8);
+            jsonBody = jsonBody.Replace(name, "Subproject13");
+        }
+    }
+}
---
+++ DesafioAPI/Tests/Projects/UpdateASubprojectTest.cs
@@ -0,0 +1,38 @@
+﻿using DesafioAPI.Helpers;
+using DesafioAPI.Requests.Projects;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharpNetCoreTemplate.Bases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioAPI.Tests.Projects
+{
+    public class UpdateASubprojectTest : TestBase
+    {
+        [Test]
+        public void AlterarSubProjeto()
+        {
+            //arrange
+
+            string name = "$nameProjectSub";
+
+            //actions
+            int project_id = 4;
+
+            int subproject_id = 7;
+
+            UpdateASubprojectRequest updateASubprojectRequest = new UpdateASubprojectRequest(project_id, subproject_id);
+            updateASubprojectRequest.SetJsonBody(name);
+
+            IRestResponse<dynamic> response = updateASubprojectRequest.ExecuteRequest();
+            
+            response.PrintResponse();
+            
+            Assert.AreEqual(System.Net.HttpStatusCode.NoContent, response.StatusCode);
+            //Assert.AreEqual("1", response.Data["id"].ToString());
+        }
+    }
+}
